47a941fe779b5674a9b9372be0473435fec0b0e5
grinner91 <mohammad.moniruzzaman@miu.edu>
2023-01-31 00:07:16 -0600

    updated w js file
    updated js file

--- w3lab1/w3lab1_class_part1.js
+++ w3lab1/w3lab1_class_part1.js
@@ -1,55 +1,46 @@
 
-//   function Clock({ template }) {
-  
-//     let timer;
-  
-//     function render() {
-//       let date = new Date();
-  
-//       let hours = date.getHours();
-//       if (hours < 10) hours = '0' + hours;
-  
-//       let mins = date.getMinutes();
-//       if (mins < 10) mins = '0' + mins;
-  
-//       let secs = date.getSeconds();
-//       if (secs < 10) secs = '0' + secs;
-  
-//       let output = template
-//         .replace('h', hours)
-//         .replace('m', mins)
-//         .replace('s', secs);
-  
-//       console.log(output);
-//     }
-  
-//     this.stop = function() {
-//       clearInterval(timer);
-//     };
-  
-//     this.start = function() {
-//       render();
-//       timer = setInterval(render, 1000);
-//     };
-  
-//   }
-  
-//   let clock = new Clock({template: 'h:m:s'});
-//   clock.start();
-
 /*
- 
+
 Q1. Rewrite to class
 
 Ans1: I did the following steps to convert a class:
 
-1. added constructor fuction 
+1. added constructor function 
 2. removed let timer variable
 3. added this.timer variable in methods
 3. removed function key words from render(), start(), stop()
 
+
+Q2: Class Inheritance (https://javascript.info/class-inheritance#tasks)
+
+Q2.1: Error creating an instance
+
+Ans2.1: error was in Rabbit constructor "supper constructor" was not called for parent animal constructor
+Called parent's super(name) constructor to fix it. 
+
+ constructor(name) {
+    super(name)
+    this.created = Date.now();
+  }
+
+Q2.2. Extended clock
+
+Ans2.2:
+1. Added a new class ExtendedClock extends Clock 
+2. Added a constructor function with params as following: 
+
+class ExtendedClock extends Clock {
+  constructor(precision, template) {
+    super(template)
+    this.precision = precision
+  }
+} 
+
+3. overrode start method to start timer with precision 
+
 */
 
+/*///////////////////////////
 class Clock {
 
     constructor({template}) {
@@ -113,37 +104,7 @@ let exclock = new ExtendedClock(5000, {template: 'h:m:s'});
 exclock.start();
 //exclock.stop();
 
-/*
-
-
-Q2: Class Inheritance (https://javascript.info/class-inheritance#tasks)
-
-Q2.1: Error creating an instance
-Ans2.1: error was in Rabbit constructor "supper constructor" was not called for parent animal constructor
-Called parent's super(name) construcutor to fix it. 
-
- constructor(name) {
-    super(name)
-    this.created = Date.now();
-  }
-
-Q2.2. Extended clock
-
-Ans2.2:
-1. Added a new class ExtendedClock extends Clock 
-2. Added a constructor function with params as following: 
-
-class ExtendedClock extends Clock {
-  constructor(pricision, template) {
-    super(template)
-    this.pricision = pricision
-  }
-} 
-
-3. overrode start memthod to start timer with pricision 
-
-*/
-
+///////////////////////////////////
 class Animal {
 
   constructor(name) {
@@ -161,6 +122,4 @@ class Rabbit extends Animal {
 
 let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
 console.log(rabbit.name);
-
-
-
+*/
