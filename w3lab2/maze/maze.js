dd10f03c66d2120e4aca50d9379b8dbb4bfe97b4
grinner91 <mohammad.moniruzzaman@miu.edu>
2023-01-31 22:00:55 -0600

    Added W3lab2 - Maze game
    Added W3lab2 - Maze game

---
+++ w3lab2/maze/maze.js
@@ -0,0 +1,67 @@
+
+let isStartedMaze = false;
+
+$(function(){
+
+    setupMazeGame();
+
+});
+
+function setupMazeGame() {
+    
+    $("#start").on('click mouseenter', function(){
+        setBoundaryEvents();
+        setEndOptionEvents();
+        //alert("start");
+    });
+
+}
+
+function setBoundaryEvents() {
+    $("div.boundary").mouseenter(function(){
+        $("div.boundary").addClass("redback");
+        $("#boundary1").text("Loser...");
+        $("div.boundary").animate({
+            "background-color": "red",
+            "color": "#fff",
+            "font-size": '32px',
+            "font-weight": 'bold'
+          }, 
+          1000,
+          resetElementsAndValues );
+        console.log("Loser.....");
+    });
+}
+
+function setEndOptionEvents() {
+    $("#end").mouseenter(function(){
+        $("div.boundary").addClass("greenback");
+        $( "#boundary1").text("Winner!!!");
+        $( "div.boundary").animate(
+            {
+            "background-color": "green",
+            "color": "#fff",
+            "font-size": '32px',
+            "font-weight": 'bold'
+          }, 
+          1000, 
+          resetElementsAndValues);
+        //alert("Winner......");
+        console.log("Winner....")
+    });
+}
+
+function resetElementsAndValues(){
+    isStartedMaze = false;
+    //reset background
+    $("div.boundary")
+    .removeClass("redback")
+    .removeClass("greenback");
+
+    //remove message 
+    $( "#boundary1").text("");
+
+    //reset event handlers 
+    $("div.boundary").off("mouseenter");
+    $("#end").off("mouseenter");
+}
