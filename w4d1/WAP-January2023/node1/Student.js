96e128a4b9b53fd364eb210cd2d55ddc6ba4f59b
grinner91 <mohammad.moniruzzaman@miu.edu>
2023-02-06 23:16:43 -0600

    updated
    updated

--- w4d1/WAP-January2023/node1/Student.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Student{
-    constructor(name, id){
-        this.name = name;
-        this.id = id;
-    }
-    studentInfo(){
-        return this.name + ", " + this.id;
-    }
-}
